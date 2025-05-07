c223ed7eea2a2f5175dbf9d188914dac4126d415
vinidev <thauanrbpc@gmail.com>
2025-05-07 00:35:30 -0300

    added endpoints for the features handlers

---
+++ src/Modules/Basket/Basket/Basket/Features/AddItemIntoBasket/AddItemIntoBasketEndpoint.cs
@@ -0,0 +1,22 @@
+﻿namespace Basket.Basket.Features.AddItemIntoBasket;
+
+public record AddItemIntoBasketRequest(ShoppingCartItemDto ShoppingCartItem);
+public record AddItemIntoBasketResponse(Guid Id);
+
+internal class AddItemIntoBasketEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/basket/{userName}/items", async ([FromRoute] string userName, [FromBody] AddItemIntoBasketRequest request, ISender sender) =>
+        {
+            var command = new AddItemIntoBasketCommand(userName, request.ShoppingCartItem);
+            var result = await sender.Send(command);
+            var response = result.Adapt<AddItemIntoBasketResponse>();
+
+            return Results.Ok(response);
+        }).Produces<AddItemIntoBasketResponse>(StatusCodes.Status200OK)
+          .ProducesProblem(StatusCodes.Status400BadRequest)
+          .WithSummary("Add Item Into Basket")
+          .WithDescription("Add Item Into Basket");
+    }
+}
---
+++ src/Modules/Basket/Basket/Basket/Features/CreateBasket/CreateBasketEndpoint.cs
@@ -0,0 +1,22 @@
+﻿namespace Basket.Basket.Features.CreateBasket;
+
+public record CreateBasketRequest(ShoppingCartDto ShoppingCart);
+public record CreateBasketResponse(Guid Id);
+
+internal class CreateBasketEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/basket", async (CreateBasketRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<CreateBasketCommand>();
+            var result = await sender.Send(command);
+            var response = result.Adapt<CreateBasketResult>();
+
+            return Results.Created($"/basket/{response.Id}", response);
+        }).Produces<CreateBasketRequest>(StatusCodes.Status201Created)
+          .ProducesProblem(StatusCodes.Status400BadRequest)
+          .WithSummary("Create basket")
+          .WithDescription("Create basket");
+    }
+}
